b55a003835dd4dfe148752eef711756d5c9e1494
Pim Vermeer <pim.vermeer@gmail.com>
2022-11-11 18:20:02 +0100

    chore: automated dependency updater workflow

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -e
+
+onlyUpdate="false"
+if [ "$1" == "only-update" ]; then
+    onlyUpdate="true"
+fi
+
+echo -e "\n"
+echo "Checking dependencies"
+echo -e "\n"
+
+if npx npm-check-updates --configFilePath ./scripts --configFileName npm-check-updates-rc.json --dep prod,dev,peer,bundle,optional --deep --errorLevel 2; then
+    echo -e "\n"
+    echo "No dependency updates found"
+    echo -e "\n"
+
+    exit 0
+else
+    echo -e "\n"
+    echo "Depency updates found"
+    echo -e "\n"
+fi
+
+echo -e "\n"
+echo "Updating all dependency versions"
+echo -e "\n"
+
+if npx npm-check-updates --configFilePath ./scripts --configFileName npm-check-updates-rc.json --dep prod,dev,peer,bundle,optional --deep -u; then
+    echo -e "\n"
+    echo "Updated all package.json's"
+    echo -e "\n"
+else
+    echo -e "\n"
+    echo "Failed"
+    echo -e "\n"
+
+    exit 1
+fi
+
+if [ "$onlyUpdate" == "true" ]; then
+    exit 0
+fi
+
+echo -e "\n"
+echo "Installing all dependencies"
+echo -e "\n"
+
+if npm run bootstrap; then
+    echo -e "\n"
+    echo "Installed new dependencies versions"
+    echo -e "\n"
+else
+    echo -e "\n"
+    echo "Failed"
+    echo -e "\n"
+
+    exit 1
+fi
+
+echo -e "\n"
+echo "Running tests"
+echo -e "\n"
+
+if npm run test; then
+    echo -e "\n"
+    echo "All tests successful"
+    echo -e "\n"
+else
+    echo -e "\n"
+    echo "Failed"
+    echo -e "\n"
+
+    exit 1
+fi

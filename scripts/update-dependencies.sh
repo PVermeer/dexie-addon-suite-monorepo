abc2c30fc1841ff6b43de67b3ef4cbc4040808c0
Pim Vermeer <pim.vermeer@gmail.com>
2022-11-12 14:10:10 +0100

    fix: manual peer dependency management

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
+# Options
 onlyUpdate="false"
 if [ "$1" == "only-update" ]; then
     onlyUpdate="true"
 fi
 
+dependencyTypes="prod,dev,bundle,optional"
+configFilePath="./scripts"
+configFileName="npm-check-updates-rc.json"
+
 echo -e "\n"
 echo "Checking dependencies"
 echo -e "\n"
 
-if npx npm-check-updates --configFilePath ./scripts --configFileName npm-check-updates-rc.json --dep prod,dev,peer,bundle,optional --deep --errorLevel 2; then
+if npx npm-check-updates --configFilePath $configFilePath --configFileName $configFileName --dep $dependencyTypes --deep --errorLevel 2; then
     echo -e "\n"
     echo "No dependency updates found"
     echo -e "\n"
@@ -26,7 +31,7 @@ echo -e "\n"
 echo "Updating all dependency versions"
 echo -e "\n"
 
-if npx npm-check-updates --configFilePath ./scripts --configFileName npm-check-updates-rc.json --dep prod,dev,peer,bundle,optional --deep -u; then
+if npx npm-check-updates --configFilePath $configFilePath --configFileName $configFileName --dep $dependencyTypes --deep -u; then
     echo -e "\n"
     echo "Updated all package.json's"
     echo -e "\n"

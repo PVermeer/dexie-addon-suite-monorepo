7cc27f4e4657f56dfa505535c376cc6d72cd7e5e
Pim Vermeer <pim.vermeer@gmail.com>
2023-01-09 13:22:00 +0000

    build: dependabot use fix commit + allow (deps) commit scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,19 @@
+// @ts-check
+
+// Extend scopes for dependabot
+const configLernaScopes = require("@commitlint/config-lerna-scopes");
+const scopeEnum = async (context) => {
+  const lernaScopes = await configLernaScopes.rules["scope-enum"](context);
+  const [level, applicable, scopes] = lernaScopes;
+  return [level, applicable, [...scopes, "deps"]];
+};
+
 module.exports = {
   extends: [
     "@commitlint/config-conventional",
     "@commitlint/config-lerna-scopes",
   ],
+  rules: {
+    "scope-enum": scopeEnum,
+  },
 };
